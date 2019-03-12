36b3f170226e108203c8839066184b00205f02f3
Rickard Eriksson <rickard.eriksson@trustly.com>
2019-03-12 14:23:32 +0100

    removed unnecessary xsfr tokens

--- bot/seed.py
+++ bot/seed.py
@@ -5,8 +5,6 @@ class SeedGenerator():
     def __init__(self):
         self.headers = {     
                     'Origin': 'https://alttpr.com',
-                    'X-XSRF-TOKEN': 'eyJpdiI6Ik5rUE1yays0TGcxU3ZlQW15bWR4REE9PSIsInZhbHVlIjoiQUFcLzhRcW9BVmRuRDBPSTl5Z1ZvTk9hc3FzZHdDamFcLzU2aElndmRWZXZTOThhcjVMak15clF4aUNWV1RLUlJtIiwibWFjIjoiMzA5NDAzNWFjYzUzNTNlMjk3NWRiZTRhZjBkYTJjMDdlYWZiZjgxYjk5NGRmNzE4MTA4NzYxNWU1NjdlMjAyMiJ9',
-                    'X-CSRF-TOKEN': 'GXGdvDspxXnaSp7BcD2Zjd33pHsE5CwfCCDQ7sUU',
                     'Accept-Encoding': 'gzip, deflate, br',
                     'Accept-Language': 'en-US,en;q=0.9,sv;q=0.8',
                     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
